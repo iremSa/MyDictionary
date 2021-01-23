9976cd63138362ac93f54683324e06f36af3bf6e
iremSa <iremsaribiyik03@outlook.com>
2021-01-24 00:24:33 +0300

    first commit

---
+++ MyDictionary/MyList.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyList<TKey, TValue>
+    {
+        TKey[] keys;
+        TValue[] values;
+        public MyList()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+
+            TKey[] tempKey = keys;
+            keys = new TKey[keys.Length + 1];
+            TValue[] tempValue = values;
+            values = new TValue[values.Length + 1];
+
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                keys[i] = tempKey[i];
+                values[i] = tempValue[i];
+            }
+
+            for (int j = 0; j < keys.Length; j++)
+            {
+                if (key == null)
+                {
+                    throw new NotImplementedException();
+                    Console.WriteLine("Key won't be null");
+                }
+                else
+                {
+
+                }
+            }
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+        public int Count
+        {
+            get { return keys.Length; }
+
+        }
+
+        public TKey[] Keys
+        {
+            get { return keys; }
+        }
+
+        public TValue[] Values
+        {
+            get { return values; }
+        }
+
+    }
+}
+
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int, string> ordernumbers = new MyDictionary.MyList<int, string>();
+
+            ordernumbers.Add(1, "İrem");
+            ordernumbers.Add(2, "Görkem");
+            ordernumbers.Add(3, "İstemihan");
+            Console.WriteLine("Count: " + ordernumbers.Count);
+            foreach (var values in ordernumbers.Values)
+            {
+                Console.WriteLine("values:" + values);
+            }
+
+
+
+        }
+    }
+}
+   
